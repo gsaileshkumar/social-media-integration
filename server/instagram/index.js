2f64e86e979a11dae1d485ba65f66475eb3f75ad
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-01-19 20:23:17 +0800

    commit

--- server/instagram/index.js
+++ server/instagram/index.js
@@ -10,20 +10,7 @@ if (process.env.NODE_ENV !== "production") {
 
 router.get("/auth", function(req, res) {
   return res.redirect(
-    // `${process.env.INSTAGRAM_OAUTH_URI}/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=${process.env.INSTAGRAM_REDIRECT_URI}&scope=user_profile,user_media&response_type=code`
-    `${process.env.INSTAGRAM_OAUTH_URI}/authorize?
-    client_id=${process.env.INSTAGRAM_CLIENT_ID}
-    &redirect_uri=${process.env.INSTAGRAM_REDIRECT_URI}
-    &scope=user_profile,user_media
-    &response_type=code`
-    // process.env.INSTAGRAM_OAUTH_URI +
-    //   "/authorize?" +
-    //   "client_id=" +
-    //   process.env.INSTAGRAM_CLIENT_ID +
-    //   "&redirect_uri=" +
-    //   process.env.INSTAGRAM_REDIRECT_URI +
-    //   "&scope=user_profile,user_media" +
-    //   "&response_type=code"
+    `${process.env.INSTAGRAM_OAUTH_URI}/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=${process.env.INSTAGRAM_REDIRECT_URI}&scope=user_profile,user_media&response_type=code`
   );
 });
 

d2b23b47f2c24a90c1495ac769edfe7fdb9048fe
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-01-19 20:55:21 +0800

    commit

--- server/server.js
+++ server/server.js
@@ -14,30 +14,29 @@ app.use(cors());
 
 app.use("/instagram", instagram);
 
-app.get("/callback", async (req, res) => {
-  console.log("/callback endpoint hit");
-  res.send("/callback endpoint hit");
-});
-
-app.get("/content", async (req, res) => {
-  const instagramMediaOptions = {
-    url: `${process.env.INSTAGRAM_GRAPH_URI}/me/media`,
-    method: "get",
-    params: {
-      fields:
-        "id,caption,permalink,media_url,timestamp,username,thumbnail_url,media_type",
-      access_token: instagramToken.get()
+app.get(
+  "/content",
+  cors({ origin: "sailesh.netlify.com" }),
+  async (req, res) => {
+    const instagramMediaOptions = {
+      url: `${process.env.INSTAGRAM_GRAPH_URI}/me/media`,
+      method: "get",
+      params: {
+        fields:
+          "id,caption,permalink,media_url,timestamp,username,thumbnail_url,media_type",
+        access_token: instagramToken.get()
+      }
+    };
+    try {
+      const instagramResponse = await axios.request(instagramMediaOptions);
+      console.log("instagram resp", instagramResponse.data);
+      res.status(200).send(instagramResponse.data);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("SERVER ERROR");
     }
-  };
-  try {
-    const instagramResponse = await axios.request(instagramMediaOptions);
-    console.log("instagram resp", instagramResponse.data);
-    res.status(200).send(instagramResponse.data);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("SERVER ERROR");
   }
-});
+);
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../index.html"));

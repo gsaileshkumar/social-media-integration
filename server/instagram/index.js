9a3814cc53258dbaed61a8d1b6f7a214f68cc5b4
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-01-19 16:18:10 +0800

    commit

--- server/instagram/index.js
+++ server/instagram/index.js
@@ -11,7 +11,7 @@ router.get("/auth", function(req, res) {
     `${process.env.INSTAGRAM_OAUTH_URI}/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=${process.env.INSTAGRAM_REDIRECT_URI}&scope=user_profile,user_media&response_type=code`
   );
 });
-router.get("/callback", function(req, res) {
+router.get("/callback", async function(req, res) {
   const { code } = req.query;
   console.log("code", code);
   if (!code) {
@@ -24,13 +24,14 @@ router.get("/callback", function(req, res) {
     redirect_uri: process.env.INSTAGRAM_REDIRECT_URI,
     code
   };
-  return axios
-    .post(`${process.env.INSTAGRAM_OAUTH_URI}/access_token`, options)
-    .then(resp => {
-      console.log(resp);
-      console.log("data", resp.data);
-      return res.send(resp);
-    });
+  const responseJson = await axios.post(
+    `${process.env.INSTAGRAM_OAUTH_URI}/access_token`,
+    options
+  );
+
+  console.log(responseJson);
+  console.log("data", responseJson.data);
+  return res.send(responseJson.data);
 });
 
 module.exports = router;

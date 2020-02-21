87395d3bea6fd6a4221aa0ddb68ec3a96190615a
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-02-22 02:54:14 +0530

    commit

--- server/server.ts
+++ server/server.ts
@@ -13,15 +13,7 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-app.use(
-  cors({
-    origin: [
-      "https://sailesh.netlify.com",
-      "http://saileshkumar.com",
-      "https://saileshkumar.com"
-    ]
-  })
-);
+app.use(cors());
 
 app.use("/instagram", instagram);
 app.use("/twitter", twitter);

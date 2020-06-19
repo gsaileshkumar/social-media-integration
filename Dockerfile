39c21c003a2439e77e5c5a62d18e3c26601654d4
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-06-20 00:40:06 +0800

    Dockerize app

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:latest
+
+WORKDIR /app
+
+# Install app dependencies
+COPY package*.json ./
+
+RUN rm -rf node_modules
+RUN npm install -g npm@latest
+RUN npm i core-util-is
+RUN npm ci --only=production --silent
+
+# Copy sources and build
+COPY . .
+
+RUN npm run build
+
+CMD ["node", "server/server.js"]
--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import path from "path";
-import cors from "cors";
-import axios from "./network";
-import instagram from "./instagram";
-import instagramToken from "./instagram/store";
-import twitter from "./twitter";
-import twitterToken from "./twitter/store";
-import { Method } from "axios";
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import axios from './network';
+import instagram from './instagram';
+import instagramToken from './instagram/store';
+import twitter from './twitter';
+import twitterToken from './twitter/store';
+import { Method } from 'axios';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,43 +15,43 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.use(cors());
 
-app.use("/instagram", instagram);
-app.use("/twitter", twitter);
+app.use('/instagram', instagram);
+app.use('/twitter', twitter);
 
-app.get("/content", async (req, res) => {
+app.get('/content', async (req, res) => {
   const instagramMediaOptions = {
     url: `${process.env.INSTAGRAM_GRAPH_URI}/me/media`,
-    method: "get" as Method,
+    method: 'get' as Method,
     params: {
       fields:
-        "id,caption,permalink,media_url,timestamp,username,thumbnail_url,media_type",
-      access_token: instagramToken.get()
-    }
+        'id,caption,permalink,media_url,timestamp,username,thumbnail_url,media_type',
+      access_token: instagramToken.get(),
+    },
   };
   const twitterOptions = {
     url: `${process.env.TWITTER_API_URI}/statuses/user_timeline.json`,
-    method: "get" as Method,
+    method: 'get' as Method,
     params: {
-      screen_name: "gsaileshkumar"
+      screen_name: process.env.TWITTER_SCREEN_NAME,
     },
     headers: {
-      Authorization: `Bearer ${twitterToken.get()}`
-    }
+      Authorization: `Bearer ${twitterToken.get()}`,
+    },
   };
   try {
     const instagramResponse = await axios.request(instagramMediaOptions);
     const twitterResponse = await axios.request(twitterOptions);
     const data = {
       instagram: instagramResponse.data,
-      twitter: twitterResponse.data
+      twitter: twitterResponse.data,
     };
     res.status(200).send(data);
   } catch (error) {
     console.log(error);
-    res.status(500).send("SERVER ERROR");
+    res.status(500).send('SERVER ERROR');
   }
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './index.html'));
 });
